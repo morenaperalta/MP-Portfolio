c7cf76b59640aaee0818b86f28d4e010528f31d9
More-Pe <peraltamorena.va@gmail.com>
2024-09-19 12:50:49 +0200

    feat: navbar and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { AiFillGithub } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+
+function Footer() {
+	return (
+		<Container
+			fluid
+			className='footer'>
+			<p>
+				Feel free to <span className='aqua'>connect </span>with me!
+			</p>
+			<ul className='home-about-social-links'>
+				<li className='social-icons'>
+					<a
+						href='https://github.com/More-Pe'
+						target='_blank'
+						rel='noopener noreferrer'
+						className='icon-colour  home-social-icons'>
+						<AiFillGithub />
+					</a>
+				</li>
+				<li className='social-icons'>
+					<a
+						href='https://www.linkedin.com/in/morena-peralta-almada/'
+						target='_blank'
+						rel='noopener noreferrer'
+						className='icon-colour  home-social-icons'>
+						<FaLinkedinIn />
+					</a>
+				</li>
+			</ul>
+		</Container>
+	);
+}
+
+export default Footer;
