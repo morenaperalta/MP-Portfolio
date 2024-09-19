19e43c7ef3507780b70353eacf438ba651d7fc7a
More-Pe <peraltamorena.va@gmail.com>
2024-09-19 13:18:05 +0200

    feat: tech stack component

---
+++ src/components/Stack/TechStack.jsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import {
+	DiJavascript1,
+	DiReact,
+	DiNodejs,
+	DiMongodb,
+	DiPython,
+	DiGit,
+	DiJava,
+	DiMysql,
+} from 'react-icons/di';
+import {
+	SiTypescript,
+	SiPhp,
+	SiLaravel,
+	SiBootstrap,
+	SiHtml5,
+	SiCss3,
+	SiTailwindcss,
+	SiGithub,
+} from 'react-icons/si';
+
+function Techstack() {
+	return (
+		<Row style={{ justifyContent: 'center', paddingBottom: '40px' }}>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<DiJavascript1 /> <h6>JavaScript</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiTypescript /> <h6>TypeScript</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiHtml5 /> <h6>HTML5</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiCss3 /> <h6>CSS3</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<DiNodejs /> <h6>Node.js</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<DiReact />
+				<h6>React.js</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiBootstrap />
+				<h6>Bootstrap</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiTailwindcss /> <h6>Tailwind</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<DiMysql />
+				<h6>MySQL</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<DiMongodb />
+				<h6>MongoDB</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<DiGit />
+				<h6>Git</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiGithub />
+				<h6>GitHub</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiPhp />
+				<h6>PHP</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiLaravel />
+				<h6>Laravel</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<DiPython />
+				<h6>Phyton</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<DiJava />
+				<h6>Java</h6>
+			</Col>
+		</Row>
+	);
+}
+
+export default Techstack;
