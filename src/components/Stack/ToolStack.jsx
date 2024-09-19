2de9cb0e1453dfbb2623ca6279b6ea06074410e2
More-Pe <peraltamorena.va@gmail.com>
2024-09-19 13:18:43 +0200

    feat: tool stack component

---
+++ src/components/Stack/ToolStack.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import {
+	SiVisualstudiocode,
+	SiPostman,
+	SiSlack,
+	SiVercel,
+	SiMacos,
+	SiTrello,
+} from 'react-icons/si';
+
+function Toolstack() {
+	return (
+		<Row style={{ justifyContent: 'center', paddingBottom: '40px' }}>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiMacos />
+				<h6>MacOS</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiVisualstudiocode />
+				<h6>VSC</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiPostman />
+				<h6>Postman</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiSlack />
+				<h6>Slack</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiTrello />
+				<h6>Trello</h6>
+			</Col>
+			<Col
+				xs={2}
+				md={2}
+				lg={1}
+				className='tech-icons'>
+				<SiVercel />
+				<h6>Vercel</h6>
+			</Col>
+		</Row>
+	);
+}
+
+export default Toolstack;
